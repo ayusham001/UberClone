66fbf3d2202c9e0cb2e08e03d304728e96bcb6db
Ayush Mishra <ayusham001@gmail.com>
2023-11-17 22:05:29 +0530

    payment integration

---
+++ components/Home/CheckoutForm.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
+
+function CheckoutForm({ amount }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (elements == null) {
+      return;
+    }
+    const { error: submitError } = await elements.submit();
+    if (submitError) {
+      return;
+    }
+    const res = await fetch('/api/create—intent', {
+      method: 'POST',
+      body: JSON.stringify({
+        amount: amount
+      })
+    })
+    const secretKey=await res.json();
+    console.log(secretKey);
+    const { error } = await stripe.confirmPayment({
+      clientSecret:secretKey,
+      elements,
+      confirmParams:{
+        return_url:"https://ubeerrr.netlify.app/"
+      }
+    })
+  }
+  return (
+    <div className='flex flex-col justify-center items-center w-full'>
+      <h2 className='m-5 font-bold'>Amount to Pay: {amount}</h2>
+      <form className='max-w-md' onSubmit={handleSubmit}>
+        <PaymentElement />
+        <button className='w-full bg-black text-white p-2 rounded-lg mt-2'>Pay</button>
+      </form>
+    </div>
+  )
+}
+
+export default CheckoutForm
